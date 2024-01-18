05417a835913ee444d000c187d32b4564596c809
Aisha-Yusuf <ayshaofficiel@gmail.com>
2024-01-18 21:12:49 +0300

    almost done

--- server/app.py
+++ server/app.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    return '\n'.join(str(i) for i in range(parameter)) + '\n'
+     
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    num1 = int(num1)
+    num2 = int(num2)
+
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1*num2
+    elif operation == 'div':
+        result = num1/num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        result = "Invalid operation"
+
+    return str(result)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
